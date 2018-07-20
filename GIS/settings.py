f0751698c3cc0370ecd519afba1282866374d851
Abdulrahman <a.aldelaigan@gmail.com>
2018-07-20 17:33:54 +0300

    Adjusted settings, url and wsg files

--- GIS/settings.py
+++ GIS/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'GIS1.urls'
+ROOT_URLCONF = 'GIS.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'GIS1.wsgi.application'
+WSGI_APPLICATION = 'GIS.wsgi.application'
 
 
 # Database
